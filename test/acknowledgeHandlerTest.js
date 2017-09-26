869459bdc2dc20bffb16e826583c1a8cfc20ff36
Sam Boyd <samboyd10@gmail.com>
2017-09-26 14:10:43 +0100

    refactor(test): Seperated suite into two to make it more visible whats being tested

--- test/acknowledgeHandlerTest.js
+++ test/acknowledgeHandlerTest.js
@@ -6,72 +6,88 @@ import { parse, handleAcknowledgement } from '../src/acknowledgeHandler'
 import { serviceLocator } from '../src/thrukServiceLocator'
 
 describe('Acknowledge Hander', () => {
-    it('should fail to parse with an error', () => {
+    describe('parse', () => {
+        it('should fail to parse with an error', () => {
             let parsed = parse('acknowledge not a valid anything');
 
             expect(parsed).to.have.property('error');
+        });
+
+        it('should correctly parse an acknowledgement', () => {
+            let parsed = parse('acknowledge Puppet run result across all exchanges on hostless-supply-side');
+
+            expect(parsed).to.have.property('service');
+            expect(parsed).to.have.property('host');
+
+            expect(parsed.service).to.equal('Puppet run result across all exchanges');
+            expect(parsed.host).to.equal('hostless-supply-side')
+        });
     });
+    
+    describe('handleAcknowledge', () => {
+        it('should respond to a with the correct msg', () => {
+            const inputText = {'text': 'acknowledge Puppet run result across all exchanges on hostless-supply-side'};
+            const bot = {
+                reply: function () {
+                }
+            };
+            const spy = sinon.spy(bot, 'reply');
 
-    it('should correctly parse an acknowledgement', () => {
-        let parsed = parse('acknowledge Puppet run result across all exchanges on hostless-supply-side');
+            handleAcknowledgement(bot, inputText);
 
-        expect(parsed).to.have.property('service');
-        expect(parsed).to.have.property('host');
+            const replyCall = bot.reply.getCall(0);
 
-        expect(parsed.service).to.equal('Puppet run result across all exchanges')
-        expect(parsed.host).to.equal('hostless-supply-side')
-    });
+            expect(replyCall.args[1]).to.equal('Service:Puppet run result across all exchanges, Host:hostless-supply-side')
+            expect(spy.calledOnce).to.be.true
+        });
 
-    it('should respond to a with the correct msg', () => {
-        const inputText = {'text': 'acknowledge Puppet run result across all exchanges on hostless-supply-side'};
-        const bot = { reply: function() {}};
-        const spy = sinon.spy(bot, 'reply');
+        it('should respond with a error if the input is not a real service', () => {
+            const inputText = {'text': 'acknowledge This is not a service on hostless-supply-side'};
+            const bot = {
+                reply: function () {
+                }
+            };
+            const spy = sinon.spy(bot, 'reply');
 
-        handleAcknowledgement(bot, inputText);
+            handleAcknowledgement(bot, inputText);
 
-        const replyCall = bot.reply.getCall(0);
+            const replyCall = bot.reply.getCall(0);
 
-        expect(replyCall.args[1]).to.equal('Service:Puppet run result across all exchanges, Host:hostless-supply-side')
-        expect(spy.calledOnce).to.be.true
-    });
+            expect(replyCall.args[1]).to.equal('I can\'t seem to find that service');
+            expect(spy.calledOnce).to.be.true
+        });
 
-    it('should respond with a error if the input is not a real service', () => {
-        const inputText = {'text': 'acknowledge This is not a service on hostless-supply-side'};
-        const bot = { reply: function() {}};
-        var spy  = sinon.spy(bot, 'reply');
-        
-        handleAcknowledgement(bot, inputText);
+        it('should respond with a error if the input is not a real host', () => {
+            const inputText = {'text': 'acknowledge Puppet run result across all exchanges on not-a-host'};
+            const bot = {
+                reply: function () {
+                }
+            };
+            const spy = sinon.spy(bot, 'reply');
 
-        const replyCall = bot.reply.getCall(0);
+            handleAcknowledgement(bot, inputText);
 
-        expect(replyCall.args[1]).to.equal('I can\'t seem to find that service');
-        expect(spy.calledOnce).to.be.true
-    });
+            const replyCall = bot.reply.getCall(0);
 
-    it('should respond with a error if the input is not a real host', () => {
-        const inputText = {'text': 'acknowledge Puppet run result across all exchanges on not-a-host'};
-        const bot = { reply: function() {}};
-        var spy  = sinon.spy(bot, 'reply');
+            expect(replyCall.args[1]).to.equal('I can\'t seem to find that host');
+            expect(spy.calledOnce).to.be.true
+        });
 
-        handleAcknowledgement(bot, inputText);
+        it('should respond with an error if the inputted service is in an OK state', () => {
+            const inputText = {'text': 'acknowledge A check in an OK state on some host'};
+            const bot = {
+                reply: function () {
+                }
+            };
+            const spy = sinon.spy(bot, 'reply');
 
-        const replyCall = bot.reply.getCall(0);
+            handleAcknowledgement(bot, inputText);
 
-        expect(replyCall.args[1]).to.equal('I can\'t seem to find that host');
-        expect(spy.calledOnce).to.be.true
-    });
+            const replyCall = bot.reply.getCall(0);
 
-    it.only('should respond with an error if the inputted service is in an OK state', () => {
-        const inputText = {'text': 'acknowledge A check in an OK state on some host'};
-        const bot = { reply: function() {}};
-        var spy  = sinon.spy(bot, 'reply');
-
-        handleAcknowledgement(bot, inputText);
-
-        const replyCall = bot.reply.getCall(0);
-
-        expect(replyCall.args[1]).to.equal('The service seems to be in an OK state.');
-        expect(spy.calledOnce).to.be.true
+            expect(replyCall.args[1]).to.equal('The service seems to be in an OK state.');
+            expect(spy.calledOnce).to.be.true
+        })
     })
 });
 
