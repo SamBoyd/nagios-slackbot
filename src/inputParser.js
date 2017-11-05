532f0b1918726080158ffac0dd9a937628ea2b15
Sam Boyd <samboyd10@gmail.com>
2017-11-05 12:13:42 +0000

    feat(downtime): Added ability to parse a schedule downtime input

--- src/inputParser.js
+++ src/inputParser.js
@@ -24,3 +24,31 @@ export const parseInputForAcknowledge = (m) => {
         }
     }
 };
+
+export const parseInputForDowntime = (m) => {
+    const patternString = /schedule downtime for (.+(?=on))on (.+(?=for))for (.+)/gi;
+    let patt = new RegExp(patternString);
+
+    let message = m;
+    let bool =  patt.test(message);
+
+    if (bool) {
+        var matches = patternString.exec(message);
+
+        if (matches.length == 0) {
+            return {
+                'error': 'Invalid acknowledgement'
+            }
+        }
+
+        return {
+            'service': matches[1].trim(),
+            'host': matches[2].trim(),
+            'duration': matches[3].trim()
+        }
+    } else {
+        return {
+            'error': 'Invalid acknowledgement'
+        }
+    }
+};
